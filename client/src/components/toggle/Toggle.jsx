c67de9ebe4cd418ea54c8c5e466255f1ccf35ea9
marphco <5993166+marphco@users.noreply.github.com>
2024-10-15 10:04:32 -0400

    fixed toggle

--- client/src/components/toggle/Toggle.jsx
+++ client/src/components/toggle/Toggle.jsx
@@ -1,26 +1,28 @@
-import "./Toggle.css"
+import './Toggle.css';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faLightbulb } from '@fortawesome/free-solid-svg-icons';
 
-export const Toggle = ({ handleChange, isChecked}) => {
-    return (
-        <div className="toggle-container">
-            <input
-            type="checkbox"
-            id="check"
-            className="toggle"
-            onChange={handleChange}
-            checked={isChecked}
-            />
-            <label htmlFor="check">
-                <FontAwesomeIcon className="toggle-icon" icon={isChecked ? faMoon : faLightbulb} size="sm" />
-            </label>
-        </div>
-    )
-}
+export const Toggle = ({ handleChange, isChecked }) => {
+  return (
+    <div className="toggle-container">
+      <div
+        className="toggle-circles"
+        onClick={handleChange}
+        role="button"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            handleChange();
+          }
+        }}
+      >
+        <div className={`circle white-circle ${!isChecked ? 'active' : ''}`}></div>
+        <div className={`circle black-circle ${isChecked ? 'active' : ''}`}></div>
+      </div>
+    </div>
+  );
+};
 
 Toggle.propTypes = {
-    handleChange: PropTypes.func.isRequired,
-    isChecked: PropTypes.bool.isRequired,
-}
+  handleChange: PropTypes.func.isRequired,
+  isChecked: PropTypes.bool.isRequired,
+};
