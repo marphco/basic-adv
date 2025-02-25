4af4699e67991b81076ec55a58887738383abc2d
marphco <5993166+marphco@users.noreply.github.com>
2025-02-24 23:06:53 -0500

    added search bar

---
+++ client/src/components/dashboard/SearchBar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    onSearch(value); // Passa il termine di ricerca al componente genitore
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Cerca per nome..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+SearchBar.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
+
+export default React.memo(SearchBar);
